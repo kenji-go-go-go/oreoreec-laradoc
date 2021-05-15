7e6463682517be43ecba422f85e975c845308aca
kenji-go-go-go <FZF01113@nifty.com>
2021-05-16 00:22:17 +0900

    カードまで実装

--- oreore-ec/routes/web.php
+++ oreore-ec/routes/web.php
@@ -14,7 +14,24 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', 'ProductController@index')->name('product.index');
+// Route::get('/', 'ProductController@index')->name('product.index');
+// Route::get('/product/{id}', 'ProductController@show')->name('product.show');
+Route::name('product.')
+    ->group(function () {
+        Route::get('/', 'ProductController@index')->name('index');
+        Route::get('/product/{id}', 'ProductController@show')->name('show');
+    });
+
+Route::name('line_item.')
+    ->group(function () {
+        Route::post('/line_item/create', 'LineItemController@create')->name('create');
+        Route::post('/line_item/delete', 'LineItemController@delete')->name('delete');
+    });
+
+Route::name('cart.')
+    ->group(function () {
+        Route::get('/cart', 'CartController@index')->name('index');
+    });
 
 Auth::routes();
 
